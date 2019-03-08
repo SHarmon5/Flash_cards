140f0046b7938beba6a0691bc960ff0204dd968b
Evan Wilson <wilsonevan100@gmail.com>
2019-03-08 16:52:27 -0700

    Final Commit

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+var showAddForm = false
+
 $(document).ready( function() {
+
   $(".card").on("click", function() {
     //console.log("here")
     let description = this.childNodes[3];
@@ -12,13 +15,57 @@ $(document).ready( function() {
 
   })
 
-  $("#btn-add-card").on("click", function() {
-    // addNewCard();
+  // Toggle the new card form
+  $('#btn-add-card').on('click', function() {
+
+    toggle();
+    
   })
 
-  $("#submit").on("click", function( title, description ) {
-    debugger
+  function toggle() {
+    showAddForm = !showAddForm
+
+    if (showAddForm) {
+      $('#btn-add-card').hide();
+      $('#add-card-form').show();
+    } else {
+      $('#btn-add-card').show();
+      $('#add-card-form').hide();
+    }
+
+  }
+
+  // Create a new flashcard at the beginning of the list of flashcards
+  function addFlashcard (title, description) {
+    var flashCards = $('.grid-container')
+
+    var div = "<div class='flash_card'><p class='title'>" + title
+    + "</p><hr /><p class='description'>" + description
+    + "</p><button id='edit'>Edit</button><button id='delete'>Delete</button></div>"
+
+    flashCards.append(div)
+    // debugger
+  }
+
+  // Process the data from the form to create a new flashcard
+  $('#submit').on('click', function(event) {
+    
+    
+    event.preventDefault();
+    var results = $('#form-new').serializeArray()
+    var title = results[0].value
+    var description = results[1].value
+    addFlashcard(title, description)
+    toggle();
   })
+
 
     
 });
+
+
+
+// function newCard() {
+//   debugger
+  
+// }
