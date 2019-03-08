d497c959c467c4490ea8abd98942ca1d36af3167
Evan Wilson <wilsonevan100@gmail.com>
2019-03-08 15:28:04 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready( function() {
+
+  $("#btn-add-card").on("click", function() {
+    // addNewCard();
+  })
+
+  $("#submit").on("click", function( title, description ) {
+    debugger
+  })
+
+  // function toggle() {
+  //   showForm = !showForm;
+  //   $("#btn-add-card").hide();
+  //   $("#").toggle();
+
+  //   if (showForm) {
+      
+  //   }
+  // }
+
+  // function addNewCard() {
+  //   // Add a new object of the card clas
+  //   // need to be able to increment # of ids by 1
+  //   Card.new()
+
+
+
+  //   var div = '<div data-id=' + newCard.id + 
+
+  // }
+    
+});
